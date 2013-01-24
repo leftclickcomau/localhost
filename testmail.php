d2d58dc70fd4992f3768af234de255daae976cb1
Ben New <ben@leftclick.com.au>
2013-01-24 09:35:58 +0000

    Add -f header to mail() call

--- testmail.php
+++ testmail.php
@@ -11,7 +11,7 @@ $headers = 'From: no-reply@leftclick.com.au' . "\r\n" . 'Content-type: text/plai
 echo '<li>' . date('Y-m-d H:i:s') . ': Ready to send</li>' . "\n";
 echo '<li>' . date('Y-m-d H:i:s') . ': Settings: <ul><li>$to = ' . $to . '</li><li>$subject = ' . $subject . '</li><li>$message = ' . substr($message, 0, 50) . '</li><li>$headers = ' . $headers . '</li></ul></li>' . "\n";
 
-$result = mail($to, $subject, $message, $headers);
+$result = mail($to, $subject, $message, $headers, "-f ben@203.161.90.233.static.amnet.net.au");
 
 echo '<li>' . date('Y-m-d H:i:s') . ': Mail sent, result = ' . ($result ? 'true' : 'false') . '</li>' . "\n";
 ?>
